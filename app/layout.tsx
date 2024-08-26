c71fa3a18898b54238e7be38c2607fe987ace139
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-08-26 13:22:40 +0300

    + header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/components/shared/index'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
@@ -21,7 +22,10 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={nunito.className}>
-				<main className='min-h-screen'>{children}</main>
+				<main className='min-h-screen'>
+					<Header />
+					{children}
+				</main>
 			</body>
 		</html>
 	)
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import { Button } from '@/components/ui/button'
 
 export default function Home() {
-	return (
-		<h1>
-			Home
-			<Button variant='outline'>Cart</Button>
-		</h1>
-	)
+	return <h1>Home</h1>
 }
